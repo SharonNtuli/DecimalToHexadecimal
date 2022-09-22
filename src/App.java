a7c510ae57fbf412458643ec0c160f7092c91e24
SharonNtuli <sharontuli315@gmail.com>
2022-09-22 15:22:39 +0200

    Update App.java

--- src/App.java
+++ src/App.java
@@ -6,7 +6,7 @@ public class App {
         //System.out.println("Hello, World!");
         Scanner scan =  new Scanner (System.in);
         int decimalNo,remainder;
-        char[]hexa={'1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+        char[]hexa={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
         
         String hexaValue="";
 
