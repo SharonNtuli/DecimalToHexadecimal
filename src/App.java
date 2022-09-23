0ec57ceb527cabd68365af7050e38121c3f25431
SharonNtuli <111461455+SharonNtuli@users.noreply.github.com>
2022-09-23 08:28:51 +0200

    Update App.java

--- src/App.java
+++ src/App.java
@@ -3,15 +3,15 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        //System.out.println("Hello, World!");
-        Scanner scan =  new Scanner (System.in);
+    
+        Scanner deci =  new Scanner (System.in);
         int decimalNo,remainder;
         char[]hexa={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
         
         String hexaValue="";
 
         System.out.print("Enter any number from 0 to 10000 :");
-        decimalNo = scan.nextInt();
+        decimalNo = deci.nextInt();
 
         if(decimalNo<=10000) 
         {
@@ -24,7 +24,7 @@ public class App {
         }
         System.out.print("Hexa value is : "+ hexaValue);
        } else {
-            // Executes when the Boolean expression is false
+          
             System.out.print("The number must be from 0 to 10000");
          }
         
